eabc221548abd7a9a84712711bcaa62dd7bff369
mihanizm56 <m727507.56@mail.ru>
2020-08-26 20:35:56 +0300

    build(configure-project): first config

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+// создание ссылки
+export const inviteLinkGenerator = (inviteToken: string) =>
+  `https://ui-root.suppliers-portal-eu.svc.k8s.test/wb-eu-invites?inviteToken=${inviteToken}`;
+
+// routePath для роутера
+export const inviteRoutePath = '/wb-eu-invites?inviteToken';
+
+// routeName для роутера
+export const invitePageName = 'wb-eu-invites';
