911506fdf5c7b06a59050716ca66bb10d0f7f713
mihanizm56 <m727507.56@mail.ru>
2021-03-11 00:11:14 +0300

    fix(services): fix export

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,2 @@
-// создание ссылки
-export const inviteLinkGenerate = (inviteToken: string) => {
-  const url = new URL(window.location.href);
-
-  const origin = url.origin;
-
-  return `${origin}/wb-eu-invites?invite_token=${inviteToken}`;
-};
-
-// routePath для роутера
-export const inviteRoutePath = '/wb-eu-invites?invite_token';
-
-// routeName для роутера
-export const invitePageName = 'wb-eu-invites';
+export { fetchLogger } from './utils/logger';
+export { LoggerMainParamsType, LoggerServiceParams } from './types';
