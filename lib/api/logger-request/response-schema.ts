13cd97e093c5fc356cd38bec4313224752205243
mihanizm56 <m727507.56@mail.ru>
2021-03-11 14:27:44 +0300

    fix(configure-project): ашч куызщтыу ысруьф

--- lib/api/logger-request/response-schema.ts
+++ lib/api/logger-request/response-schema.ts
@@ -1,15 +1,5 @@
 import Joi from '@hapi/joi';
 
 export const responseSchema = Joi.object({
-  formattedResponse: Joi.string()
-    .required()
-    .allow(undefined)
-    .allow(null),
   id: Joi.string().required(),
-  request: Joi.string().required(),
-  response: Joi.string()
-    .required()
-    .allow(undefined)
-    .allow(null),
-  type: Joi.string().required(),
 });
